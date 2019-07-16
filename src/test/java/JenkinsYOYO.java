5924ca5e408dc545ef583ff3449bdad0fff67405
Kubanychbek Tumonbaev <kast@Kubanychbeks-MacBook-Pro.local>
2019-07-15 20:36:08 -0500

    first jenkins commit

---
+++ src/test/java/JenkinsYOYO.java
@@ -0,0 +1,14 @@
+public class JenkinsYOYO {
+
+    public static void main(String[] args) {
+
+        String love = "I Love Jenkins))";
+
+        for(int i=0; i<10; i++){
+            System.out.println(love);
+        }
+
+
+    }
+
+}
